e54155d1ad729e5c63f704d5d6b73605debd2b0c
Guilherme <guilherme.waess@gmail.com>
2018-03-20 21:56:15 +0100

    wip:  work on password component

---
+++ src/components/shared/Password.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-text-field name="password"
+                label="Enter your password"
+                hint="At least 6 characters"
+                v-model="value"
+                min="6"
+                :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                :append-icon-cb="changePasswordVisibility"
+                :type="showPassword ? 'password' : 'text'"
+                counter>
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'Password',
+  props: {
+    value: { required: true },
+  },
+  data() {
+    return {
+      showPassword: false,
+    };
+  },
+  methods: {
+    changePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    },
+  },
+};
+</script>
