77e8861dbb6f2bb42f34c87a38d4f2ddacd3ef57
Guilherme <guilherme.waess@gmail.com>
2018-04-15 17:26:29 +0200

    cleanning main.js

--- src/main.js
+++ src/main.js
@@ -1,60 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import {
-  Vuetify,
-  VApp,
-  VBtn,
-  VCard,
-  VDivider,
-  VFooter,
-  VForm,
-  VList,
-  VGrid,
-  VIcon,
-  VNavigationDrawer,
-  VTextField,
-  VToolbar,
-  transitions,
-} from 'vuetify';
-
 import Vue from 'vue';
 import VeeValidate from 'vee-validate';
-import Snotify from 'vue-snotify';
 
 import App from './App';
 import router from './router';
 import store from './store';
-import '../node_modules/vuetify/src/stylus/app.styl';
-
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VBtn,
-    VCard,
-    VDivider,
-    VFooter,
-    VForm,
-    VList,
-    VGrid,
-    VIcon,
-    VNavigationDrawer,
-    VTextField,
-    VToolbar,
-    transitions,
-  },
-  theme: {
-    primary: '#1A237E',
-    secondary: '#304FFE',
-    accent: '#5C6BC0',
-    error: '#B71C1C',
-    warning: '#ffeb3b',
-    info: '#2196f3',
-    success: '#4caf50',
-  },
-});
+import './vuetify.config';
+import './snotify.config';
 
 Vue.use(VeeValidate);
-Vue.use(Snotify);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
