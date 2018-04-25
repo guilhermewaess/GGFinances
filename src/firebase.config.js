f78abdee9bfd2d78f86235c9b49d34ed791846cf
Guilherme <guilherme.waess@gmail.com>
2018-04-25 22:30:24 +0200

    hidding fbconfig

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAINNAwivw4vxAtClXc_7XHdOas_OIQVnc',
-  authDomain: 'ggfinancehomolog.firebaseapp.com',
-  databaseURL: 'https://ggfinancehomolog.firebaseio.com',
-  projectId: 'ggfinancehomolog',
-  storageBucket: 'ggfinancehomolog.appspot.com',
-  messagingSenderId: '761229836216',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
 };
